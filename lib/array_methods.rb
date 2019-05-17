2847d83a19a8788a69363c52dc0110cdb9a9f9e3
James Gomez <james.gomez320@yahoo.com>
2019-05-17 14:34:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,6 +6,8 @@ array = ["wow", "I", "am", "really", "learning", "arrays!"]
   end
 
 def using_sort(array)
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+sorted_array = array.sort
 
 end
 
